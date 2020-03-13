3cf51dbbcb84c34298e64bf21a30aa1acd3e638a
Michael Mennis <mmennis@gmail.com>
2020-03-13 17:26:09 +0000

    Add flag icon beside language selection

--- src/components/localization/LanguageSelector.js
+++ src/components/localization/LanguageSelector.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { LanguageContext } from './LanguageContext'
+import { locales, LanguageContext } from './LanguageContext'
 import { Form } from 'react-bulma-components'
+import ReactCountryFlag from 'react-country-flag'
 
 export default class LanguageSelector extends React.Component {
 
@@ -8,16 +9,21 @@ export default class LanguageSelector extends React.Component {
         return(
             <LanguageContext.Consumer>
                 { value => (
-                    <Form.Field horizontal={true}>
-                        <Form.Label size="small">Lang:</Form.Label>
-                        <Form.Control>
-                            <Form.Select size="small" color="primary" marginless={true} value={value.lang} onChange={value.updateLocale} data-testid="select">
-                                <option value="en">English</option>
-                                <option value="es">Espanol</option>
-                                <option value="fr">Francais</option>
-                            </Form.Select>
-                        </Form.Control>
-                    </Form.Field>
+                    <div>
+                        
+                        <Form.Field horizontal={true}>
+                            <Form.Label size="small">Lang:</Form.Label>
+                            <Form.Control>
+                                <Form.Select size="small" color="primary" marginless={true} value={value.lang} onChange={value.updateLocale} data-testid="select">
+                                    <option value="en">English</option>
+                                    <option value="es">Espanol</option>
+                                    <option value="fr">Francais</option>
+                                </Form.Select>
+                            </Form.Control>                        
+                            <ReactCountryFlag countryCode={locales[value.lang].flag} svg style={{ 'height': '2em', 'width': '2em' }}/>
+                        </Form.Field>
+                    </div>
+                    
                 )}
             </LanguageContext.Consumer>
         )
